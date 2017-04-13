ef5e4555c30a5d79fb80f1d0afa00f2af7ee1dbe
AlienWars <alienwarsthegame@gmail.com>
2017-04-13 17:11:39 -0300

    Add files via upload
    Teste do script de movimentação básica

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    bool noChao = false;
+    public Transform chaoCheck;
+    float chaoRadius = 0.2f;
+    public LayerMask oQueEChao;
+
     Rigidbody2D rgbd2d;
     Animator anim;
-    private float playerSpeed = 2f;
-    private float jumpHeight = 300f;
+    private float playerSpeed = 1f;
+    private float jumpHeight = 150f;
 
 	// Use this for initialization
 	void Start () {
+        //identifica animator e rigidbody2d
         rgbd2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void FixedUpdate()
+    {
+        //Cria o radius para o ChaoCheck
+        noChao = Physics2D.OverlapCircle(chaoCheck.position, chaoRadius, oQueEChao);
+        anim.SetBool("walking", noChao);
+    }
+
+        // Update is called once per frame
+        void Update () {
+
+        //Pega o valor absoluto da axis Horizontal e seta como speed, se speed for > 0.1 no animator ele começa a animação
         anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        //parte da movimentação básica do jogador
         if(Input.GetAxis("Horizontal") > 0)
         {
             transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
@@ -28,10 +45,21 @@ public class PlayerController : MonoBehaviour {
             transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
             transform.eulerAngles = new Vector2(0, 180);
         }
-        if (Input.GetButtonDown("Jump"))
+        //pulo do jogador com o chaocheck
+        if (noChao && Input.GetButtonDown("Jump"))
         {
             rgbd2d.AddForce(Vector2.up * jumpHeight);
         }
+
+        if(anim.GetBool("walking") == true)
+        {
+            anim.SetBool("jumping", false);
+        }
+        if (anim.GetBool("walking") == false)
+        {
+            anim.SetBool("jumping", true);
+        }
+
 
     }
 }
